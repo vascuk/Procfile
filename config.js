ff5a336ae33971d31bf5dc51b3760c5a537ef15f
vascuk <vascuk.r@nemk.ukr.education>
2026-04-18 13:19:03 +0300

    Change BOT_TOKEN in config.js
    Updated BOT_TOKEN for the application.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    BOT_TOKEN: '7507768571:AAFMlmNOIyPGoeGZcn5iUay3mi3vTLIxeAA',
+    BOT_TOKEN: '7507768571:AAHJh3maFhVHwE1Eb02oDE_JZf6B1wU7udk',
     
     NOVOVOLYNSK: {
         latitude: 50.7258,

515372330b120993e301c33e8c504ccdb5ff15fe
vascuk <vascuk.r@nemk.ukr.education>
2026-04-18 12:30:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 module.exports = {
-    // Токен бота від @BotFather
-    BOT_TOKEN: 'BOT_TOKEN',
+    BOT_TOKEN: process.env.BOT_TOKEN,
     
-    // Координати Нововолинська
     NOVOVOLYNSK: {
         latitude: 50.7258,
         longitude: 24.1626
